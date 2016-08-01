4652bb2f49fd9a7a50bc97bbc39061a33cc82786
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-08-01 23:22:21 +0900

    add_error_message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post])
-    @post.save
-    redirect_to @post
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -18,8 +22,7 @@ class PostsController < ApplicationController
   end
 
   private
-
-    def params
+    def post_params
       params.require(:post).permit(:title, :text)
     end
 
