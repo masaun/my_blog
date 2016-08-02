f687522b4891be9effd7fc26e9e50c85bda64a61
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-08-02 12:28:54 +0900

    add_error_edit_update_function_fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
   end
 
   def create
@@ -19,6 +20,20 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
